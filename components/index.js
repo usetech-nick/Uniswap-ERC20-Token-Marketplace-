56b6591f9ebbd9841e3bef9d1d9c3c0c1589012b
Nick <nkg9033@gmail.com>
2025-05-19 16:10:23 +0000

    Initial commit: project setup with components and architecture

---
+++ components/index.js
@@ -0,0 +1,38 @@
+import Header from "./Header";
+import Footer from "/Footer";
+import Card from "./Card";
+import HeroSection from "./HeroSection";
+import Selector from "./Selector";
+import SingleCard from "./SengleCard";
+import SwapComponent from "./SwapComponent";
+import Table from "./Table";
+import TokenBalance from "./TokenBalance";
+import TransactionStatus from "./TransactionStatus";
+
+//SVG ICONS
+
+import Menu from "./SVG/Menu";
+import Logo from "./SVG/Logo";
+import Facebook from "./SVG/Facebook";
+import Insta from "./SVG/Insta";
+import Twitter from "./SVG/Twitter";
+
+export {
+  Header,
+  Footer,
+  Card,
+  HeroSection,
+  Selector,
+  SingleCard,
+  SwapComponent,
+  Table,
+  TokenBalance,
+  TransactionStatus,
+
+  //SVG
+  Menu,
+  Logo,
+  Twitter,
+  Insta,
+  Facebook,
+};
